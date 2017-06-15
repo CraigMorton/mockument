3c335e89b4b3814e5d3b80fb8295783da1bb0d1f
Craig Morton <CraigMorton22@gmail.com>
2017-06-15 14:16:57 +0100

    Rename variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import {readFileSync} from 'fs'
 
-export default function mockDoc(pathToHtmlFile) {
-  const htmlFile = readFileSync(pathToHtmlFile)
-  const htmlDocument = document.implementation.createHTMLDocument();
-  htmlDocument.documentElement.innerHTML = htmlFile
-  return htmlDocument.body
+export default function mockumentBody(path) {
+  const htmlFile = readFileSync(path)
+  const mockument = document.implementation.createHTMLDocument()
+  mockument.documentElement.innerHTML = htmlFile
+  return mockument.body
 }
